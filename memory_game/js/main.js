1320d13c8e301a73ec237a533f31ddfcaf3f2112
Nathan Fee <nathan_fee@Yahoo.com>
2019-01-23 10:41:40 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,28 +3,33 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay =[];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2)
+var checkForMatch = function()
 {
-	if (cardsInPlay[0] === cardsInPlay[1])
+	if (cardsInPlay.length === 2)
 	{
-		alert("You found a match!");
-	}
-	else
-	{
-		alert("Sorry, try again.");
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{
+			alert("You found a match!");
+		}
+		else
+		{
+			alert("Sorry, try again.");
 
+		}
 	}
-}
+} // End checkForMatch()
+
+var flipCard = function (cardId)
+{
+	console.log("User flipped " + cards[cardId] + ".");
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+	
+} // End flipCard()
+
+flipCard(0);
+flipCard(2);
+
 
