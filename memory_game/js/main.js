6dff20dd66b34502337b192a3d3716bae20b1a73
Nathan Fee <nathan_fee@Yahoo.com>
2019-01-24 06:18:54 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",	
+}
+];
 
 var cardsInPlay =[];
 
@@ -21,9 +42,11 @@ var checkForMatch = function()
 
 var flipCard = function (cardId)
 {
-	console.log("User flipped " + cards[cardId] + ".");
+	console.log("User flipped " + cards[cardId].rank + ".");
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 	
